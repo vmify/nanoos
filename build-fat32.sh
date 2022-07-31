4f9647ce01cd17cfa88d6137c02e5086e7465c07
Axel Fontaine <axel@boxfuse.com>
2022-07-31 17:54:04 +0200

    Unified GRUB usage across architectures

--- build-fat32.sh
+++ build-fat32.sh
@@ -18,25 +18,15 @@ mount -t auto -o loop $img image
 
 mkdir -p image/EFI/BOOT
 
-if [ "$ARCH" = "x64" ]; then
-  mkdir -p image/legal/gummiboot
-  echo "https://git.alpinelinux.org/aports/tree/main/gummiboot?id=0be04d7926dfdcab6901ef28fdc11534cba7a07e" > image/legal/gummiboot/source
-  echo "48.1-r2" > image/legal/gummiboot/version
-  cp gummiboot.license image/legal/gummiboot/LICENSE
+mkdir -p image/legal/grub
+cp grub/SOURCE image/legal/grub/source
+cp grub/VERSION image/legal/grub/version
+cp grub/LICENSE image/legal/grub/LICENSE
 
-  boot=bootx64.efi
-else
-  mkdir -p image/legal/grub
-  echo "https://git.alpinelinux.org/aports/tree/main/grub?id=1895cf9fc22dde29d848d62c20eb0276ea2d34a7" > image/legal/grub/source
-  echo "2.06-r2" > image/legal/grub/version
-  cp grub.license image/legal/grub/LICENSE
-
-  sed -e "s/\$NANOOS_VERSION/$NANOOS_VERSION/g" grub.cfg > image/EFI/BOOT/grub.cfg
-  cp kernel/bzImage image/EFI/BOOT/linux
-  cp initramfs.cpio.gz image/EFI/BOOT/initrd
-  boot=bootaa64.efi
-fi
-cp $efi image/EFI/BOOT/$boot || exit
+sed -e "s/\$NANOOS_VERSION/$NANOOS_VERSION/g" grub.cfg > image/EFI/BOOT/grub.cfg
+cp kernel/bzImage image/EFI/BOOT/linux
+cp initramfs.cpio.gz image/EFI/BOOT/initrd
+cp grub/*.efi image/EFI/BOOT || exit
 
 mkdir -p image/legal/kernel
 echo "https://github.com/vmify/kernel/archive/refs/tags/$KERNEL_VERSION.tar.gz" > image/legal/kernel/source
--- build.cmd
+++ build.cmd
@@ -7,13 +7,12 @@ set NANOOS_VERSION=0.0.0
 
 FOR /F "tokens=*" %%i in ('type dependencies.conf') do SET %%i
 
-REM curl -L -o kernel.tar.gz https://github.com/vmify/kernel/releases/download/%KERNEL_VERSION%/kernel-minimal-%ARCH%-%KERNEL_VERSION%.tar.gz
-curl -L -o busybox.tar.gz https://github.com/vmify/busybox/releases/download/%BUSYBOX_VERSION%/busybox-minimal-%ARCH%-%BUSYBOX_VERSION%.tar.gz
+curl -L -C - -o grub.tar.gz https://github.com/vmify/grub/releases/download/%GRUB_VERSION%/grub-%ARCH%-%GRUB_VERSION%.tar.gz
+curl -L -C - -o kernel.tar.gz https://github.com/vmify/kernel/releases/download/%KERNEL_VERSION%/kernel-minimal-%ARCH%-%KERNEL_VERSION%.tar.gz
+curl -L -C - -o busybox.tar.gz https://github.com/vmify/busybox/releases/download/%BUSYBOX_VERSION%/busybox-minimal-%ARCH%-%BUSYBOX_VERSION%.tar.gz
 
 docker buildx build --platform=linux/%DOCKER_ARCH% --build-arg ARCH=%ARCH% --build-arg NANOOS_VERSION=%NANOOS_VERSION% --build-arg KERNEL_VERSION=%KERNEL_VERSION% --build-arg BUSYBOX_VERSION=%BUSYBOX_VERSION% --progress=plain --output type=local,dest=. -f Dockerfile-initramfs .
-docker buildx build --platform=linux/%DOCKER_ARCH% --build-arg ARCH=%ARCH% --build-arg NANOOS_VERSION=%NANOOS_VERSION% --build-arg KERNEL_VERSION=%KERNEL_VERSION% --progress=plain --output type=local,dest=. -f Dockerfile-efi .
-
-docker buildx build --build-arg ARCH=%ARCH% --build-arg NANOOS_VERSION=%NANOOS_VERSION% --build-arg KERNEL_VERSION=%KERNEL_VERSION% --build-arg BUSYBOX_VERSION=%BUSYBOX_VERSION% --progress=plain -t fat32-build -f Dockerfile-fat32 .
+docker buildx build --build-arg ARCH=%ARCH% --build-arg NANOOS_VERSION=%NANOOS_VERSION% --build-arg GRUB_VERSION=%GRUB_VERSION% --build-arg KERNEL_VERSION=%KERNEL_VERSION% --build-arg BUSYBOX_VERSION=%BUSYBOX_VERSION% --progress=plain -t fat32-build -f Dockerfile-fat32 .
 docker run --privileged --name fat32-run fat32-build
 docker cp fat32-run:/nanoos.fat32.gz .
 docker container rm fat32-run
